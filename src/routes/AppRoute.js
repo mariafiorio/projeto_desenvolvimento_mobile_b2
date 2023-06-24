4065cf9ebba37b74ff34e45ec2a1503f2ed7b454
Maria <103320974+mariafiorio@users.noreply.github.com>
2023-06-23 21:24:38 -0300

    Create AppRoute.js

---
+++ src/routes/AppRoute.js
@@ -0,0 +1,14 @@
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import MoviesPage from "../pages/MoviesPage";
+import MoviesDetails from "../pages/MoviesDetailsPage";
+
+const Stack = createNativeStackNavigator();
+
+export default function AppRoutes(){
+    return(
+        <Stack.Navigator>
+            <Stack.Screen name="MoviesPage" component={MoviesPage} options={{headerShown: false}}/>
+            <Stack.Screen name="MoviesDetailsPage" component={MoviesDetailsPage} options={{headerShown: true}}/>
+        </Stack.Navigator>
+    )
+}
